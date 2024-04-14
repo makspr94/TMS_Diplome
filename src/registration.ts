6b68bc28359da037702c3dcb65452c614fb90b1e
maksim.prybylski <makspr94@gmail.com>
2024-04-15 00:32:42 +0300

    test are done

--- src/registration.ts
+++ src/registration.ts
@@ -1,65 +1,65 @@
 import { error } from "console";
 
-
 export class RegistrationForm {
-    
-    email: string;
-    password: string;
-    username: string;
-    age: number;
-    termsAgreement: boolean = false;
-    registered: boolean = false;
+  email: string;
+  password: string;
+  username: string;
+  age: number;
+  termsAgreement: boolean = false;
+  registered: boolean = false;
 
+  setEmail(email: string) {
+    const emailRegEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; //allowed symbols in the name: . _ % + -
+    if (emailRegEx.test(email)) {
+      this.email = email;
+    } else {
+      throw new Error("incorrect Email format");
+    }
+  }
 
-    setEmail (email:string){
-        const emailRegEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ //allowed symbols in the name: . _ % + -
-         if (emailRegEx.test(email)){
-            this.email = email
-         }
-         else{
-            throw new Error('incorrect Email format');
-         }
+  setPassword(password: string) {
+    if (password.length >= 8 && /\d/.test(password)) {
+      this.password = password;
+    } else {
+      throw new Error(
+        "The password must contain at least 8 characters including at least one digit",
+      );
     }
+  }
 
-    setPassword(password: string){
-        if (password.length >= 8 && /\d/.test(password)){
-            this.password = password
-        }
-        else{
-            throw new Error ('The password must contain at least 8 characters including at least one digit');
-        }
+  setUsername(username: string) {
+    if (username.trim() !== "") {
+      this.username = username;
+    } else {
+      throw new Error("username field must be filled");
     }
+  }
 
-    setUsername(username: string) {
-        if (username.trim() !== ""){
-            this.username = username
-        }
-        else{
-            throw new Error ('username field must be filled')
-        }
+  setAge(age: number| string) {
+    if (typeof(age) == 'number' && (age > 0 && age < 150)) {
+      this.age = age;
+    } else {
+      throw new Error("incorrect age");
     }
-    
-    setAge(age: number){
-        if (age > 0  && age <150 ){
-            this.age = age;
-        }
-        else {
-            throw new Error ('incorrect age')
-        }
-    }
+  }
 
-    agreedWithTerms(){
-        this.termsAgreement = true;
-    }
+  agreedWithTerms() {
+    this.termsAgreement = true;
+  }
 
-    register(){
-        if (this.email && this.password && this.username && this. age && this.termsAgreement){
-            this.registered = true;
-            const registrationDate = new Date().toLocaleString();
-            return `Registration is successful. Date of registration: ${registrationDate}`
-        }
-        else{
-            return "registration is not allowed. Please, check fields"
-        }
+  register() {
+    if (
+      this.email &&
+      this.password &&
+      this.username &&
+      this.age &&
+      this.termsAgreement
+    ) {
+      this.registered = true;
+      const registrationDate = new Date().toLocaleString();
+      return `Registration is successful. Date of registration: ${registrationDate}`;
+    } else {
+      return "registration is not allowed. Please, check fields";
     }
+  }
 }
--- tests/registration.test.ts
+++ tests/registration.test.ts
@@ -1,109 +1,125 @@
-
 import { RegistrationForm } from "../src/registration";
 
-describe( "registration form tests", () => {
-    let registrationForm: RegistrationForm
-    beforeEach(()=> {
-     registrationForm = new RegistrationForm();
-    })
-    
-    
+describe("registration form tests", () => {
+  let registrationForm: RegistrationForm;
+  beforeEach(() => {
+    registrationForm = new RegistrationForm();
+  });
 
-    // - Пользователь может получить все посты
-    test('set email', async () => {
-        const testEmail = 'Correct.%_+-Email90@mail.com'
-        registrationForm.setEmail(testEmail);
-        expect(registrationForm.email).toEqual(testEmail);
-       
+  test("set email", async () => {
+    const testEmail = "Correct.%_+-Email90@mail.com";
+    registrationForm.setEmail(testEmail);
+    expect(registrationForm.email).toEqual(testEmail);
+  });
+
+  test("NEGATIVE. incorrect email", async () => {
+    const invalidEmails = [
+      "incorrectEmail#@mail.com",
+      "@mail.com",
+      "symbol$@mail.com",
+      "emailMail.com",
+      "почта.почта.бел",
+      "email@mail.r",
+      "email@.com",
+      "",
+      //'Correct.%_+-Email90@mail.com'
+    ];
+    invalidEmails.forEach((invalidEmail) => {
+      expect(() => {
+        registrationForm.setEmail(invalidEmail);
+      }).toThrow("incorrect Email format");
     });
+  });
 
-    test ('NEGATIVE. incorrect email', async () =>{
-        const invalidEmails =[ 
-            'incorrectEmail#@mail.com',
-            '@mail.com',
-            'symbol$@mail.com',
-            'emailMail.com',
-            'почта.почта.бел',
-            'email@mail.r',
-            'email@.com',
-            '',
-            //'Correct.%_+-Email90@mail.com'
-        ];
-        //registrationForm.setEmail(testEmail)
-        invalidEmails.forEach((invalidEmail)=> {
-            expect (()=> {
-                registrationForm.setEmail(invalidEmail);
-            }).toThrow("incorrect Email format");
-        })
-     })
-
-     test('set password', async () => {
-        const testPasswords = [
-            '8symbols',
-            'русские8',
-            '!@#$%^&8',
-            '12345678'
-        ];
-        testPasswords.forEach((password)=>{
-            registrationForm.setPassword(password);
-            expect(registrationForm.password).toEqual(password);
-        })
+  test("set password", async () => {
+    const testPasswords = ["8symbols", "русские8", "!@#$%^&8", "12345678"];
+    testPasswords.forEach((password) => {
+      registrationForm.setPassword(password);
+      expect(registrationForm.password).toEqual(password);
     });
+  });
 
-     test ('NEGATIVE. set password', async () =>{
-        const invalidPasswords =[ 
-            '7symbol',
-            'symbolsOnly',
-            'символы%%%',
-            '1234567',
-            '!@#$%^',
-            '',
-            //'Correct.%_+-Email90@mail.com'
-        ];
-        //registrationForm.setEmail(testEmail)
-        invalidPasswords.forEach((password)=> {
-            expect (()=> {
-                registrationForm.setPassword(password);
-            }).toThrow('The password must contain at least 8 characters including at least one digit');
-        })
-     });
-
-     test('set userName', async () => {
-        const testPasswords = [
-            'UserName',
-            'Имя',
-            'Hello World',
-            '        Name     '
-        ];
-        testPasswords.forEach((name)=>{
-            registrationForm.setUsername(name);
-            expect(registrationForm.username).toEqual(name);
-        })
+  test("NEGATIVE. set password", async () => {
+    const invalidPasswords = [
+      "7symbol",
+      "symbolsOnly",
+      "символы%%%",
+      "1234567",
+      "!@#$%^",
+      "",
+    ];
+    invalidPasswords.forEach((password) => {
+      expect(() => {
+        registrationForm.setPassword(password);
+      }).toThrow(
+        "The password must contain at least 8 characters including at least one digit",
+      );
     });
+  });
 
-    test ('NEGATIVE. set password', async () =>{
-        const invalidPasswords =[ 
-            '7symbol',
-            'symbolsOnly',
-            'символы%%%',
-            '1234567',
-            '!@#$%^',
-            '',
-            //'Correct.%_+-Email90@mail.com'
-        ];
-        //registrationForm.setEmail(testEmail)
-        invalidPasswords.forEach((password)=> {
-            expect (()=> {
-                registrationForm.setPassword(password);
-            }).toThrow('The password must contain at least 8 characters including at least one digit');
-        })
-     })
-    
+  test("set userName", async () => {
+    const userNames = ["UserName", "Имя", "Hello World", "        Name     "];
+    userNames.forEach((name) => {
+      registrationForm.setUsername(name);
+      expect(registrationForm.username).toEqual(name);
+    });
+  });
 
-})
+  test("NEGATIVE. set userName", async () => {
+    const invalidUserNames = [""];
+    invalidUserNames.forEach((name) => {
+      expect(() => {
+        registrationForm.setUsername(name);
+      }).toThrow("username field must be filled");
+    });
+  });
 
+  test("set age", async () => {
+    const userNames = [1, 55, 149];
+    userNames.forEach((age) => {
+      registrationForm.setAge(age);
+      expect(registrationForm.age).toEqual(age);
+    });
+  });
 
+  test("NEGATIVE. set age", async () => {
+    const invalidUserNames = [-1, 0, 150, 151, '', '12', 'hello'];
+    invalidUserNames.forEach((age) => {
+      expect(() => {
+        registrationForm.setAge(age);
+      }).toThrow("incorrect age");
+    });
+  });
 
+  test("set agreed with Terms", async () => {
+   registrationForm.agreedWithTerms();
+   expect(registrationForm.termsAgreement).toBeTruthy();
+  });
 
+  test("NEGATIVE. set age", async () => {
+    expect(registrationForm.termsAgreement).toBeFalsy();
+  });
 
+  test("registration", async () => {
+    registrationForm.email = 'correctEmail@mail.com';
+    registrationForm.password = "password1";
+    registrationForm.username = "userName";
+    registrationForm.age = 20;
+    registrationForm.termsAgreement = true;
+    const registrationDate = new Date().toLocaleString();
+    expect(registrationForm.register()).toEqual(`Registration is successful. Date of registration: ${registrationDate}`);
+    expect(registrationForm.termsAgreement).toBeTruthy();
+   });
 
+   test("NEGATIVE. registration -> missed email", async () => {
+    registrationForm.email = '';
+    registrationForm.password = "password1";
+    registrationForm.username = "userName";
+    registrationForm.age = 20;
+    registrationForm.termsAgreement = true;
+    const registrationDate = new Date().toLocaleString();
+    expect(registrationForm.register()).toEqual(`registration is not allowed. Please, check fields`);
+    expect(registrationForm.termsAgreement).toBeTruthy();
+   });
+
+});
