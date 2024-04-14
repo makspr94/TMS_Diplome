ea82dae651066d05395e49ed8730c61e63fdafde
maksim.prybylski <makspr94@gmail.com>
2024-04-14 22:25:52 +0300

    registration class is ready

---
+++ src/registration.ts
@@ -0,0 +1,65 @@
+import { error } from "console";
+
+
+export class RegistrationForm {
+    
+    email: string;
+    password: string;
+    username: string;
+    age: number;
+    termsAgreement: boolean = false;
+    registered: boolean = false;
+
+
+    setEmail (email:string){
+        const emailRegEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+         if (emailRegEx.test(email)){
+            this.email = email
+         }
+         else{
+            throw error('incorrect Email format');
+         }
+    }
+
+    setPassword(password: string){
+        if (password.length >= 8 && /\d/.test(password)){
+            this.password = password
+        }
+        else{
+            throw error ('The password must contain at least 8 characters including at least one digit')
+        }
+    }
+
+    setUsername(username: string) {
+        if (username.trim() !== ""){
+            this.username = username
+        }
+        else{
+            throw error ('username field must be filled')
+        }
+
+    }
+    setAge(age: number){
+        if (age > 0  && age <150 ){
+            this.age = age;
+        }
+        else {
+            throw error ('incorrect age')
+        }
+    }
+
+    agreedWithTerms(){
+        this.termsAgreement = true;
+    }
+
+    register(){
+        if (this.email && this.password && this.username && this. age && this.termsAgreement){
+            this.registered = true;
+            const registrationDate = new Date().toLocaleString();
+            return `Registration is successful. Date of registration: ${registrationDate}`
+        }
+        else{
+            return "registration is not allowed. Please, check fields"
+        }
+    }
+}
